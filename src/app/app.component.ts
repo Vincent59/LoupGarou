37cf4d0a85b76f3194b29000c62074b90808a6b7
Lucie Dhz <luciedher@gmail.com>
2017-04-12 09:38:06 +0200

    verif bouton commencer la partie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,30 @@ export class AppComponent implements OnInit{
   private joueurs : Joueur[] = [];
   private currentJoueur : Joueur = new Joueur();
 
+  private start = false;
+
   ngOnInit(): void {
 
     this.socket = io('http://localhost:3005')
 
     this.socket.on('getJoueurs', function (data) {
-          var tmp = data.tabJoueur;
           this.joueurs = data.tabJoueur;
+          
+          if(this.joueurs.length >= 6) //peut on lancer la partie
+          {
+              this.start = true;
+          }
+
     }.bind(this));
   }
 
   addJoueur(pseudo){
     this.currentJoueur.setPseudo(pseudo);
     this.socket.emit('newJoueur', { joueur : this.currentJoueur });
+
+    if(this.joueurs.length >= 6)
+    {
+        this.start = true;
+    }
   }
 }
