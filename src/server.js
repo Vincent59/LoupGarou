f5cc4f8693389e033fddc9440f4faedafd49db28
Maxime Detaille <maxime.detaille@gmail.com>
2017-04-12 13:49:39 +0200

    Fix problème multi co

--- src/server.js
+++ src/server.js
@@ -10,19 +10,27 @@ http.listen(PORT, () => console.log('Le serveur tourne'));
 tabJoueur = [];
 
 io.sockets.on('connection', function (socket) {
+  console.log("Nouvelle connection")
   socket.emit('getJoueurs', {tabJoueur: tabJoueur});
 
   socket.on('newJoueur', function (data) {
     var present = false;
+    ipClient = socket.conn.remoteAddress;
+    data.joueur.ip = ipClient;
     tabJoueur.forEach(function(e){
+      if(e.ip==data.joueur.ip){
+        present=true;
+        socket.emit('erreurIp',{message : "Cette ip est déjà utilisée."})
+      }
       if(e.pseudo==data.joueur.pseudo){
         present=true;
-        socket.emit('erreurDoublon', {message : "Joueur déjà présent"});
+        socket.emit('erreurDoublon', {message : "Joueur déjà présent."});
       }
     })
     if(!present){
       tabJoueur.push(data.joueur);
     }
-    io.emit('getJoueurs', {tabJoueur: tabJoueur});
+    socket.emit('getJoueurs', {tabJoueur: tabJoueur});
+    io.emit('joueurs', {tabJoueur: tabJoueur});
   });
 });
